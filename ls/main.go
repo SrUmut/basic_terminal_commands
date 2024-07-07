4b08e91d0087889585fa54c81dc8e9f223b62369
SrUmut <srumutozkan@gmail.com>
2024-07-07 17:07:41 +0300

    added -l -h -a for ls

--- ls/main.go
+++ ls/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func checkError(err error) {
@@ -12,25 +14,107 @@ func checkError(err error) {
 	}
 }
 
+func getHumanReadable(byteSize int64) string {
+	var hr string
+	byteSizeF := float32(byteSize)
+	if byteSizeF >= 1125899906842624 { // PB
+		hr = fmt.Sprintf("%.1fP", byteSizeF/1099511627776)
+	} else if byteSizeF >= 1099511627776 { // TB
+		hr = fmt.Sprintf("%.1fT", byteSizeF/1099511627776)
+	} else if byteSizeF >= 1073741824 { // GB
+		hr = fmt.Sprintf("%.1fG", byteSizeF/1073741824)
+	} else if byteSizeF >= 1048576 { // MB
+		hr = fmt.Sprintf("%.1fM", byteSizeF/1048576)
+	} else if byteSizeF >= 1024 { // KB
+		hr = fmt.Sprintf("%.1fK", byteSizeF/1024)
+	} else {
+		hr = fmt.Sprintf("%vB", byteSize)
+	}
+	return hr
+}
+
 func main() {
 	argc, argv := len(os.Args), os.Args
-	path := ""
+	path := "./"
+	longFormat := false // -l
+	listAll := false    // -a
+	humanReadable := false
 	if argc > 1 {
-		path = argv[1]
-	} else {
-		path = "./"
+		for _, arg := range argv[1:] {
+			if strings.HasPrefix(arg, "-") { // if argument is a flag, exp: -l
+				for _, flag := range arg[1:] {
+					switch flag {
+					case 'l':
+						longFormat = true
+					case 'a':
+						listAll = true
+					case 'h':
+						humanReadable = true
+					default:
+						fmt.Printf("Error: no flag as %v\n", flag)
+						os.Exit(1)
+					}
+				}
+			} else { // if argument is a dir path
+				_, err := os.Stat(arg)
+				checkError(err)
+				path = arg
+			}
+		}
 	}
 	list, err := os.ReadDir(path)
 	checkError(err)
 	for _, entry := range list {
-		/*if entry.IsDir() {
-			fmt.Println("\033[36m" + entry.Name() + "\033[0m")
-		} else {
-			//fmt.Println(entry.Name())
-		}*/
+		entryName := ""
+		entryPerms := ""
+		var entrySize int64 = 0
+
 		filePath := path + "/" + entry.Name()
 		fileInfo, err := os.Stat(filePath)
 		checkError(err)
-		fmt.Println(fileInfo.Mode().Perm(), entry.Name())
+
+		// -l
+		if longFormat {
+			if entry.IsDir() {
+				entryPerms = "d" + fileInfo.Mode().Perm().String()[1:]
+			} else {
+				entryPerms = fileInfo.Mode().Perm().String()
+			}
+			fmt.Printf("%v\t", entryPerms)
+
+			// Entry Size
+			entrySize = fileInfo.Size()
+			if humanReadable {
+				fmt.Printf("%6v\t", getHumanReadable(entrySize))
+			} else {
+				fmt.Printf("%v\t", entrySize)
+			}
+
+			// Modification Time
+			modTime := fileInfo.ModTime()
+			fmt.Printf("%v ", modTime.Format("Jan"))
+			fmt.Printf("%2v ", modTime.Format("2"))
+			if time.Now().Year()-modTime.Year() >= 1 {
+				fmt.Printf("%5v\t", modTime.Format("2006"))
+			} else {
+				fmt.Printf("%5v\t", modTime.Format("15:04"))
+			}
+		}
+		// -a
+		if !listAll && strings.HasPrefix(entry.Name(), ".") {
+			continue
+		}
+
+		// Setting entry name
+		if entry.IsDir() { // directory
+			entryName = "\033[36m" + entry.Name() + "\033[0m"
+		} else { // file
+			if strings.Contains(entryPerms, "x") { // executable
+				entryName = "\x1b[31m" + entry.Name() + "\x1b[0m"
+			} else { // not executable
+				entryName = entry.Name()
+			}
+		}
+		fmt.Printf("%v\n", entryName)
 	}
 }
